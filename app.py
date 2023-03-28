ce35f4c5c8c7f28307c67e44fd5a208d97be1f44
ja007c1 <127996439+ja007c1@users.noreply.github.com>
2023-03-27 21:06:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 counter = 0
+numbers = []
 
 @app.route("/")
 def index():
     global counter
+    global numbers
+    
     counter += 1
+    numbers.append(random.randint(1, 1000000))
+    
     return f"Hola, mundo! Esta página ha sido vista {counter} veces"
+
+@app.route("/numbers")
+def show_numbers():
+    global numbers
+    
+    return str(numbers)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
